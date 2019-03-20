2fa9b376d5d454f487ebd3e7b72cb6fc99415be2
Gavin Kistner <gavin@phrogz.net>
2019-03-20 10:50:43 -0600

    Properly reset to best when temp is 0/false

--- index.js
+++ index.js
@@ -31,34 +31,33 @@ module.exports = function ({
     while (iterations++ <= maxIterations) {
         currentTemp = getTemp(currentTemp, iterations);
 
-        // TODO: don't reset multiple times in a row
-        if (currentTemp<=1e-6) {
-            currentState = clone(bestState);
-            currentScore = bestScore;
-            continue;
-        } else {
-            currentState = newState(lastState);
-            currentScore = getScore(currentState);
-        }
-
-        const deltaScore = currentScore.score - lastScore.score;
-        if ((deltaScore<0) || (Math.random()<=Math.exp(-deltaScore/currentTemp))) {
-            lastState = currentState;
-            lastScore = currentScore;
-
-            if (currentScore.score<=bestScore.score) {
-                bestState = clone(currentState);
-                bestScore = currentScore;
-            }
-        }
-
         if (occasionallyInvoke && invokeEvery && (iterations % invokeEvery)===0) {
             const now = new Date;
             occasionallyInvoke(lastState, lastScore, bestState, bestScore, currentTemp, iterations, (now-startTime)/1000);
         }
+
+        if (!currentTemp) {
+            lastState = currentState = clone(bestState);
+            lastScore = currentScore = bestScore;
+        } else {
+            currentState = newState(lastState);
+            currentScore = getScore(currentState);
+
+            const deltaScore = currentScore.score - lastScore.score;
+            if ((deltaScore<0) || (Math.random()<=Math.exp(-deltaScore/currentTemp))) {
+                lastState = currentState;
+                lastScore = currentScore;
+
+                if (currentScore.score<=bestScore.score) {
+                    bestState = clone(currentState);
+                    bestScore = currentScore;
+                }
+            }
+        }
     }
 
-    return {state:bestState, score:bestScore};
+    const elapsed = (new Date - startTime)/1000;
+    return {state:bestState, score:bestScore, elapsed:elapsed};
 
     function clone(state) {
         return cloneState ? cloneState.call(state, state) : state;
